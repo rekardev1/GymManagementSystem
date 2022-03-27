7269b665216ac117e019f45c79a878b277e5f2cc
Rekar <rekarpc98@gmail.com>
2022-03-27 10:24:44 +0300

    ziad krdni table Member w Employee

---
+++ Data/Tables/Employee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(50) NOT NULL, 
+    [Salary] MONEY NOT NULL, 
+    [JobType] NVARCHAR(50) NOT NULL, 
+    [PhoneNumber1] NVARCHAR(11) NOT NULL, 
+    [PhoneNumber2] NVARCHAR(11) NOT NULL
+)
---
+++ Data/Tables/Member.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Member]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL,
+	[Birthdate] Datetime2 not null, 
+    [Gender] NVARCHAR(10) NOT NULL, 
+    [Address] NVARCHAR(50) NOT NULL, 
+    [PhoneNumber1] NVARCHAR(11) NOT NULL, 
+    [PhoneNumber2] NVARCHAR(11) NOT NULL
+	
+)
