fe1ca637687be6e78ed7efea4d138f2a03ae5af6
Rekar <rekarpc98@gmail.com>
2022-03-27 10:53:18 +0300

    barnamakaman das pe dakat ba home form

--- GMSUI/Program.cs
+++ GMSUI/Program.cs
@@ -1,3 +1,5 @@
+using GMSUI.Forms;
+
 namespace GMSUI;
 
 internal static class Program {
@@ -9,6 +11,6 @@ internal static class Program {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        Application.Run(new HomeForm());
     }
 }
