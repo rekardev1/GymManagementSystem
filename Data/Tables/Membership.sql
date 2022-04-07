c11da9c46c28d027ce038f0629b545d1786289f9
Rekar <rekarpc98@gmail.com>
2022-04-07 22:44:46 +0300

    ziad krdni table i Membership w MembershipType

---
+++ Data/Tables/Membership.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Membership]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [MemberId] INT NOT NULL, 
+    [TrainerId] INT NOT NULL, 
+    [MembershipTypeId] INT NOT NULL, 
+    [ExpirationDate] DATETIME2 NOT NULL, 
+    [SignedByManagerId] INT NOT NULL, 
+    CONSTRAINT [FK_Membership_Member] FOREIGN KEY (MemberId) REFERENCES Member(Id),
+    CONSTRAINT [FK_Membership_Employee] FOREIGN KEY (TrainerId) REFERENCES Employee(Id),
+    CONSTRAINT [FK_Membership_Employee2] FOREIGN KEY (SignedByManagerId) REFERENCES Employee(Id),
+    CONSTRAINT [FK_Membership_MembershipType] FOREIGN KEY (MembershipTypeId) REFERENCES MembershipType(Id)
+)
---
+++ Data/Tables/MembershipType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[MembershipType]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Start] TIME NOT NULL, 
+    [End] TIME NOT NULL
+)
