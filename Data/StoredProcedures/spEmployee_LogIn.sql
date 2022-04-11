fa8cd5410f5daf3b7910e1a2449bae87fa209785
Rekar <rekarpc98@gmail.com>
2022-04-11 23:42:21 +0300

    ziadkrdni stored procedure i spEmployee_LogIn bo check krdni username w password
    1 aneretawa agar usernamw password rast bwn

---
+++ Data/StoredProcedures/spEmployee_LogIn.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spEmployee_LogIn]
+	@Username varchar(250),
+	@Password varchar(250)
+AS
+begin
+
+	select 1 
+	from dbo.Employee 
+	where Username = @Username and [Password] = @Password;
+
+end
