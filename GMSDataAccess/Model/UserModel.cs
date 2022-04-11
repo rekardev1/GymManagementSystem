67e82a9388209a81261ebfc5e184c9efb82b8852
Rekar <rekarpc98@gmail.com>
2022-04-11 19:57:26 +0300

    ziadkrdni UserModel

---
+++ GMSDataAccess/Model/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMSDataAccess.Model;
+public class UserModel {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public decimal Salary { get; set; }
+    public string JobType { get; set; }
+    public string PhoneNumber1 { get; set; }
+    public string PhoneNumber2 { get; set; }
+
+}
