374a09728533bbd7f1d6d1e7760e4324294451bf
Rekar <rekarpc98@gmail.com>
2022-04-11 21:10:47 +0300

    gorini FirstName w LastName ba Name wa ziad krdni Password

--- Data/Tables/Employee.sql
+++ Data/Tables/Employee.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
 	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Address] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Password] NVARCHAR(100) NOT NULL, 
+    [Address] NVARCHAR(100) NOT NULL, 
     [Salary] MONEY NOT NULL, 
     [JobType] NVARCHAR(50) NOT NULL, 
     [PhoneNumber1] NVARCHAR(11) NOT NULL, 
     [PhoneNumber2] NVARCHAR(11) NOT NULL
+    
 )
