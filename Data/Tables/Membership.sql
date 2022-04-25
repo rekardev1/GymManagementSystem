59f5fb98cffe678b0c10b18e6c750582b29c0493
Rekar <rekarpc98@gmail.com>
2022-04-25 10:59:13 +0300

    ziadkrdni user eki defullt ka admin a agar sarata hich userekman nabw la database ka

--- Data/Tables/Membership.sql
+++ Data/Tables/Membership.sql
@@ -7,6 +7,6 @@
     [ExpirationDate] DATETIME2 NOT NULL, 
     [UserId] INT NOT NULL, 
     CONSTRAINT [FK_Membership_Member] FOREIGN KEY (MemberId) REFERENCES Member(Id),
-    CONSTRAINT [FK_Membership_User] FOREIGN KEY ([UserId]) REFERENCES [User](Id),
+    
     CONSTRAINT [FK_Membership_MembershipType] FOREIGN KEY (MembershipTypeId) REFERENCES MembershipType(Id), 
 )
