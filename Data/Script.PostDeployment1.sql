987fd9c343183c9fb67ea338beb4276b99c2bc05
Rekar <rekarpc98@gmail.com>
2022-05-27 14:24:51 +0300

    Bug fixes

--- Data/Script.PostDeployment1.sql
+++ Data/Script.PostDeployment1.sql
@@ -1,8 +1,8 @@
 ﻿if not exists (select 1 from dbo.[User])
 begin
 
-    insert into dbo.[User]([Name], [Address], Salary, PhoneNumber1, PhoneNumber2, [Password], [Role])
-    values ('Admin', 'NoWhere', 0, '', '', 12345, 'Admin')
+    insert into dbo.[User]([Name], [Address], PhoneNumber1, PhoneNumber2, [Password], [Role])
+    values ('Admin', 'NoWhere', '', '', 12345, 'Admin')
 
 end
 
